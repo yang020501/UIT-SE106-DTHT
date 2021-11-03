3732499546357e0212e0d558d7e1ec3093e8302f
yang020501 <thaiduong020501@gmail.com>
2021-11-03 15:49:39 +0700

    update doRegex

--- ConvertLanguage/ConvertLanguage/Form1.Designer.cs
+++ ConvertLanguage/ConvertLanguage/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace ConvertLanguage
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
@@ -50,9 +51,13 @@ namespace ConvertLanguage
             this.btnSave = new System.Windows.Forms.Button();
             this.openFile = new System.Windows.Forms.OpenFileDialog();
             this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
             this.flowLayoutPanel2.SuspendLayout();
@@ -149,6 +154,10 @@ namespace ConvertLanguage
             // 
             this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3});
             this.toolStrip1.Location = new System.Drawing.Point(0, 28);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(1047, 29);
@@ -169,9 +178,9 @@ namespace ConvertLanguage
             // 
             this.tableLayoutPanel3.BackColor = System.Drawing.SystemColors.ControlLight;
             this.tableLayoutPanel3.ColumnCount = 3;
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel3.Controls.Add(this.rtxInput, 0, 0);
             this.tableLayoutPanel3.Controls.Add(this.rtxOutput, 2, 0);
             this.tableLayoutPanel3.Controls.Add(this.flowLayoutPanel2, 1, 0);
@@ -187,30 +196,32 @@ namespace ConvertLanguage
             // rtxInput
             // 
             this.rtxInput.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rtxInput.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rtxInput.Location = new System.Drawing.Point(3, 3);
             this.rtxInput.Name = "rtxInput";
-            this.rtxInput.Size = new System.Drawing.Size(465, 509);
+            this.rtxInput.Size = new System.Drawing.Size(324, 509);
             this.rtxInput.TabIndex = 0;
             this.rtxInput.Text = "";
+            this.rtxInput.WordWrap = false;
             // 
             // rtxOutput
             // 
             this.rtxOutput.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rtxOutput.Location = new System.Drawing.Point(578, 3);
+            this.rtxOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxOutput.Location = new System.Drawing.Point(443, 3);
             this.rtxOutput.Name = "rtxOutput";
-            this.rtxOutput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
-            this.rtxOutput.Size = new System.Drawing.Size(466, 509);
+            this.rtxOutput.Size = new System.Drawing.Size(601, 509);
             this.rtxOutput.TabIndex = 1;
             this.rtxOutput.Text = "";
+            this.rtxOutput.WordWrap = false;
             // 
             // flowLayoutPanel2
             // 
             this.flowLayoutPanel2.Controls.Add(this.btnConvert);
             this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(474, 3);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(333, 3);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(98, 509);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(104, 509);
             this.flowLayoutPanel2.TabIndex = 2;
             // 
             // btnConvert
@@ -264,6 +275,33 @@ namespace ConvertLanguage
             // 
             this.openFile.FileName = "openFileDialog1";
             // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(29, 26);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(29, 26);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(29, 26);
+            this.toolStripButton3.Text = "toolStripButton3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -280,6 +318,8 @@ namespace ConvertLanguage
             this.tableLayoutPanel2.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel3.ResumeLayout(false);
             this.flowLayoutPanel2.ResumeLayout(false);
@@ -311,6 +351,9 @@ namespace ConvertLanguage
         private System.Windows.Forms.Button btn;
         private System.Windows.Forms.ToolStripMenuItem rr4ToolStripMenuItem;
         private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
     }
 }
 
--- ConvertLanguage/ConvertLanguage/doRegex.cs
+++ ConvertLanguage/ConvertLanguage/doRegex.cs
@@ -10,48 +10,72 @@ namespace ConvertLanguage
     public static class doRegex
     {
         // xoá khoảng trắng
-        public static void clearSpace(ref string  txt)
+        public static void clearSpace(ref string txt)
         {
-            txt.Trim();
+
             txt = Regex.Replace(txt, @"\s+", "");
-           
+
         }
-        public static  string tab(int x)
+        public static string tab(int x)
         {
             string s = "\n";
-            for (int i=0;i<x;i++)
+            for (int i = 0; i < x; i++)
             {
-                s+="\t";
+                s += "\t";
             }
             return s;
         }
-        // hàm cắt dòng dòng khai báo implicit
+        // hàm cắt thành phần trong main(khai baó)
         public static string[] doMain(string txt) // txt là 1 chuỗi các loại khai báo trong Implicit
         {
-            doRegex.clearSpace(ref txt); // xoá space
-            MatchCollection Name = Regex.Matches(txt, @"(?<name>(\w)+|\S)"); // cắt tên hàm
-            MatchCollection Type = Regex.Matches(txt, @"(?<type>(\w+:((\w\*|\w))))"); // cắt các biến 
+
+            clearSpace(ref txt);
+            MatchCollection Name = Regex.Matches(txt, @"((\w)+|\S)"); // cắt tên hàm
+            MatchCollection Type = Regex.Matches(txt, @"((\w)+:(\w)+\**)"); // cắt các biến 
             string[] result = new string[1 + Type.Count];
-            for (int i = 0; i < Type.Count; i++)
+            Console.WriteLine(Type.Count);
+            result[0] = Name[0].Value;
+            for (int i = 1; i <= Type.Count; i++)
             {
-                if (i == 0)
-                    result[i] = Name[0].Groups["name"].ToString();
-                else
-                    result[i] = Type[i - 1].Value.ToString();
+
+                result[i] = Type[i - 1].Value; // or có thể tạo gr type nhưng kết quả ý chang
             }
             return result;
 
         }
-        public static string[] doPre(string txt) // txt là một chuỗi Pre
+        // hàm cắt dòng main (khai báo)
+        public static string cutMain(string txt)
         {
-            MatchCollection Pre = Regex.Matches(txt, @"(?<type>((\w)+(\W)+(\w)+)|\W|(\W)+)");
-            string[] result = new string[Pre.Count];
-            for (int i = 0; i < Pre.Count; i++)
-            {
-                result[i] = Pre[i].Value.ToString(); 
-            }
+            clearSpace(ref txt); // clear toàn bộ space và \n
+            MatchCollection main = Regex.Matches(txt, @".*?(?=pre)");
+            return txt = main[0].Value; 
 
-            return result;
         }
+        //hàm cắt dòng pre 
+        public static string cutPre(string txt)
+        {
+            clearSpace(ref txt);
+            MatchCollection pre = Regex.Matches(txt, @"pre(.*?)post"); // do match có trùng pre,post nên lấy group khi chạy regextester.exe
+            return txt = pre[0].Groups[1].Value;
+        }
+        //Hàm cắt dòng post
+        public static string cutPost(string txt)
+        {
+            clearSpace(ref txt);
+            MatchCollection post = Regex.Matches(txt, @"post(.+)");
+            return txt = post[0].Groups[1].Value; // do match có trùng post nên lấy group khi chạy regextester.exe
+        }
+
+        //public static string[] doPre(string txt) // txt là một chuỗi Pre
+        //{
+        //    MatchCollection Pre = Regex.Matches(txt, @"(?<type>((\w)+(\W)+(\w)+)|\W|(\W)+)");
+        //    string[] result = new string[Pre.Count];
+        //    for (int i = 0; i < Pre.Count; i++)
+        //    {
+        //        result[i] = Pre[i].Value.ToString(); 
+        //    }
+
+        //    return result;
+        //}
     }
 }
